4ff5f5498c68181de97ef071988f0b12fcb3c597
931-Albu-Flaviu <72073884+931-Albu-Flaviu@users.noreply.github.com>
2021-10-19 14:12:04 +0300

    something went wrong

--- Lab3+/program/PIF.py
+++
@@ -1,9 +0,0 @@
-class ProgramInternalForm:
-    def __init__(self):
-        self.pif = []
-
-    def add(self, token, st_position):
-        self.pif.append((token, st_position))
-
-    def __str__(self):
-        return str(self.pif)
