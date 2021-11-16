b3bfdc37ad1c922aa2928f46855e982151d1556b
Albu <albuflaviu17@gmail.com>
2021-11-16 14:36:39 +0200

    Week8

---
+++ parser/grammar.py
@@ -0,0 +1,36 @@
+from typing import Set, Dict, List
+
+
+class Grammar:
+    non_terminals : Set[str]
+    terminals : Set[str]
+    productions: Dict[str,List[str]]
+    starting_symbol: str
+
+
+    def __init__(self,non_terminals,terminals, productions,starting_symbol):
+        self.non_terminals = non_terminals
+        self.terminals = terminals
+        self.productions = productions
+        self.starting_symbol = starting_symbol
+
+    @staticmethod
+    def parseFile(self, filename):
+        with open(filename, "r") as file:
+            non_terminals=set([nt.strip() for nt in file.readline().strip().split(',')])
+            terminals = set([nt.strip() for nt in file.readline().strip().split(',')])
+            starting_symbol = file.readline().strip()
+            for line in file:
+                symbol, rules =[el.strip() for el in line.strip().split('->')]
+                rules = set([rule.strip()for rule in line.split('|')])
+                for rule in rules:
+                    for res in rule.split(' '):
+                        if res not in terminals and res not in non_terminals:
+                            raise SyntaxError(
+                                "Rule element not present in either terminals or in non_terminals element: " + res
+                            )
+                        
+
+
+
+
